adcda62458dc308989ed941512813ca86eb729d2
Andrew Foote <footeandrew1@gmail.com>
2022-09-25 12:33:49 +0100

    fix missing megahorn page bug

--- generators/move_list.py
+++ generators/move_list.py
@@ -5,9 +5,7 @@ from reborndb import generate
 
 def run():
     moves = (json.loads(move) for move, in DB.H.execscript('generators/move_list.sql'))
-
-    for move in moves:
-        generate.render_template('moves.html', 'move_list.jinja2', moves=moves)
+    generate.render_template('moves.html', 'move_list.jinja2', moves=moves)
 
 if __name__ == '__main__':
     run()
